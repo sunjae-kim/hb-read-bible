2a7d44e761fa17dfc912c77137d49aa07e04c536
edu-jason-kim <edu.jason.kim@gmail.com>
2025-01-03 14:26:10 +0900

    메타데이터 업데이트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,53 @@
 import { SplashLayout } from '@/components/auth/SplashLayout'
+import { Metadata, Viewport } from 'next'
 import { Toaster } from 'react-hot-toast'
 import './globals.css'
+
+export const metadata: Metadata = {
+  title: '한빛공동체',
+  description: '한빛교회 공동체가 함께하는 성경읽기',
+  openGraph: {
+    title: '한빛공동체',
+    description: '한빛교회 공동체가 함께하는 성경읽기',
+    images: [
+      {
+        url: '/images/og-image.png',
+        width: 1200,
+        height: 600,
+        alt: '한빛교회 공동체 성경읽기',
+      },
+    ],
+    siteName: '한빛교회 공동체 성경읽기',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '한빛공동체',
+    description: '한빛교회 공동체가 함께하는 성경읽기',
+    images: ['/images/og-image.png'],
+  },
+  other: {
+    'apple-mobile-web-app-title': '한빛공동체',
+    'apple-mobile-web-app-capable': 'yes',
+    'apple-mobile-web-app-status-bar-style': 'default',
+    'mobile-web-app-capable': 'yes',
+    'format-detection': 'telephone=no',
+  },
+  icons: {
+    apple: [
+      { url: '/icons/icon-192.png', sizes: '192x192' },
+      { url: '/icons/icon-512.png', sizes: '512x512' },
+    ],
+  },
+  manifest: '/manifest.json',
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  themeColor: '#ffffff',
+  userScalable: false,
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -11,12 +58,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.css"
           as="style"
         />
-        <title>한빛공동체</title>
-        <meta name="description" content="한빛공동체" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        <meta property="og:title" content="한빛공동체" />
-        <meta property="og:description" content="한빛교회 공동체 성경읽기표" />
+        <meta name="apple-mobile-web-app-title" content="한빛공동체" />
       </head>
       <body>
         <SplashLayout>{children}</SplashLayout>
